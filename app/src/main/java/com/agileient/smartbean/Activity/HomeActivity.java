ba1db8d9b22349c0d216d6e0f3c0de8633ecc5e6
Rahul <rsrahulofficial@gmail.com>
2020-03-16 05:25:19 -0700

    added new code

---
+++ app/src/main/java/com/agileient/smartbean/Activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.agileient.smartbean.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agileient.smartbean.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
